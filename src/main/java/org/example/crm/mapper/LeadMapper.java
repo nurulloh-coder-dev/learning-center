9649c98ec04d00ebdd137eb8132c6deaf2dc48b9
developer <nurilloh54@gmail.com>
2026-08-21 15:19:00 +0500

    level added

--- src/main/java/org/example/crm/mapper/LeadMapper.java
+++ src/main/java/org/example/crm/mapper/LeadMapper.java
@@ -3,10 +3,12 @@ package org.example.crm.mapper;
 import org.example.crm.annotation.IgnoreAuditFields;
 import org.example.crm.entity.dto.lead.LeadCreateDto;
 import org.example.crm.entity.dto.lead.LeadDto;
+import org.example.crm.entity.dto.lead.LeadUpdateDto;
 import org.example.crm.entity.model.Lead;
 import org.example.crm.projection.LeadProjection;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface LeadMapper {
@@ -18,4 +20,7 @@ public interface LeadMapper {
     Lead toEntity(LeadCreateDto createDto);
 
     LeadDto toDto(Lead lead);
+
+    @IgnoreAuditFields
+    void mapUpdate(@MappingTarget Lead lead, LeadUpdateDto updateDto);
 }
