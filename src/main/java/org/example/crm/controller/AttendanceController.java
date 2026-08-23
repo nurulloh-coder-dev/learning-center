ee243075ac36fde09b41195b1aefafa0bba1a709
developer <nurilloh54@gmail.com>
2026-08-23 13:57:23 +0500

    Validated annotation added to AttendanceController.java

--- src/main/java/org/example/crm/controller/AttendanceController.java
+++ src/main/java/org/example/crm/controller/AttendanceController.java
@@ -12,12 +12,14 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/attendance")
+@Validated
 public class AttendanceController {
 
     private final AttendanceService service;
