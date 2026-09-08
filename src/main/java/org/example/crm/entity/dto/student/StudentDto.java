5f33443fa615e159f562f4f2400cfd104a385382
muhammadali <boronking03@gmail.com>
2026-09-08 17:27:26 +0500

    fixed student me api

--- src/main/java/org/example/crm/entity/dto/student/StudentDto.java
+++ src/main/java/org/example/crm/entity/dto/student/StudentDto.java
@@ -1,8 +1,6 @@
 package org.example.crm.entity.dto.student;
 
-import io.swagger.v3.oas.annotations.media.Schema;
 import org.example.crm.entity.dto.user.UserDto;
-import org.example.crm.entity.enums.EnrollmentPaymentStatus;
 
 import java.math.BigDecimal;
 
--- src/main/java/org/example/crm/service/StudentService.java
+++ src/main/java/org/example/crm/service/StudentService.java
@@ -5,7 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.example.crm.entity.dto.student.StudentDto;
 import org.example.crm.entity.dto.student.StudentUpdateDto;
 import org.example.crm.entity.dto.student.StudentCreateDto;
-import org.example.crm.entity.model.Enrollment;
 import org.example.crm.entity.model.Invoice;
 import org.example.crm.entity.model.Student;
 import org.example.crm.entity.model.User;
@@ -127,8 +126,7 @@ public class StudentService extends AbstractService<
     }
 
     public Invoice getLatestInvoice(@NotNull String studentId) {
-        Invoice invoice = repository.findLatestInvoiceByStudentId(studentId)
+        return repository.findLatestInvoiceByStudentId(studentId)
                 .orElseThrow(() -> new RestException(ErrorType.INVOICE_NOT_FOUND, ErrorCodes.NotFound));
-        return invoice;
     }
 }
