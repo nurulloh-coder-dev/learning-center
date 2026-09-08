35337f1439069d7a9f0592c42eb0247420a74fde
developer <nurilloh54@gmail.com>
2026-09-08 12:52:57 +0500

    clean-ups

--- src/main/java/org/example/crm/mapper/InvoiceMapper.java
+++ src/main/java/org/example/crm/mapper/InvoiceMapper.java
@@ -5,35 +5,17 @@ import org.example.crm.entity.dto.InvoiceCreateDto;
 import org.example.crm.entity.dto.InvoiceDto;
 import org.example.crm.entity.dto.InvoiceUpdateDto;
 import org.example.crm.entity.dto.enrollment.EnrollmentDto;
-import org.example.crm.entity.enums.EnrollmentPaymentStatus;
 import org.example.crm.entity.enums.InvoiceStatus;
-import org.example.crm.entity.enums.InvoiceType;
 import org.example.crm.entity.model.*;
-import org.example.crm.exceptions.ErrorCodes;
-import org.example.crm.exceptions.ErrorType;
-import org.example.crm.exceptions.RestException;
-import org.example.crm.projection.EnrollmentMonthlyFeeProjection;
 import org.example.crm.projection.InvoiceProjection;
-import org.example.crm.repository.EnrollmentRepository;
-import org.example.crm.repository.GroupRepository;
-import org.example.crm.service.EnrollmentService;
-import org.example.crm.service.GroupService;
 import org.example.crm.service.InvoiceNumberService;
-import org.example.crm.service.LessonService;
-import org.example.crm.validator.EnrollmentValidator;
-import org.example.crm.validator.StudentValidator;
 import org.springframework.stereotype.Component;
-
-import java.math.BigDecimal;
-import java.math.RoundingMode;
-import java.time.LocalDateTime;
 
 @Component
 @RequiredArgsConstructor
 public class InvoiceMapper {
     final InvoiceNumberService invoiceNumberService;
     final EnrollmentMapper enrollmentMapper;
-    private final EnrollmentRepository enrollmentRepository;
 
 
     public Invoice toEntity(InvoiceCreateDto createDto) {
