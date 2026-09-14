fbad0603b7caccd2e9ad98cd569d970644f7b5b4
muhammadali <boronking03@gmail.com>
2026-09-15 01:19:15 +0500

    fixed user delete parts

--- src/main/java/org/example/crm/service/UserService.java
+++ src/main/java/org/example/crm/service/UserService.java
@@ -25,8 +25,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.security.SecureRandom;
 import java.util.List;
-import java.util.Optional;
-
 @Service
 public class UserService extends AbstractService<
         UserRepository,
